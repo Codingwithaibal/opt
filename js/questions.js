21620216e3d9a30a4b8cd05922a015be57aae4dc
Codingwithaibal <80753486+Codingwithaibal@users.noreply.github.com>
2021-08-16 18:22:01 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,85 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "ശീമോൻ പത്രോസ്‌ വലതുകാത്‌ വെട്ടിയ  മഹാപുരോഹിതന്റെ ദാസന്റെ പേർ/",
+    answer: " മല് ക്കൊസ്‌/Malchus",
+    options: [
+      "Hyper Text Preprocessor",
+      " മല് ക്കൊസ്‌/Malchus",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "തബീഥാ എന്ന പേരിന്റെ അർത്ഥം",
+    answer: "പേടമാൻ",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "പേടമാൻ",
+      "h"
+    ]
+  },
+    {
+    numb: 3,
+    question: "തിന്മ പ്രവർത്തിക്കുന്നവൻ പകയ്ക്കുന്നത്‌ എന്തിനെ?",
+    answer: "വെളിച്ചത്തെ",
+    options: [
+      "Hometext Preprocessor",
+      "Hypertext Programming",
+      "വെളിച്ചത്തെ",
+      "Hypertext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+    
+  },
+  {
+  numb: 6,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]  
+
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+     numb: 7,
+     question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+     options: [
+       "Option 1",
+       "option 2",
+      "option 3",
+       "option 4"
+     ]
+   },
+];
